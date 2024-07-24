d3c0f276394dcd9e7553c750561ff1f77ab06eae
Bogdan <cristea.ibogdan@gmail.com>
2024-07-24 16:05:31 +0300

    refactored interceptor to open the translated exception messages from the backend

--- src/app/interceptors/http-error.interceptor.ts
+++ src/app/interceptors/http-error.interceptor.ts
@@ -9,17 +9,18 @@ import {
 import { Observable } from 'rxjs';
 import { finalize, tap } from 'rxjs/operators';
 import { DataService } from '../services/data.service';
-import { ErrorResponse } from '../components/models/error-response.model';
 import { MatDialog } from '@angular/material/dialog';
 import { SpinnerComponent } from '../components/shared/spinner/spinner.component';
 import { TranslateService } from '@ngx-translate/core';
+import { LanguageService } from '../services/language.service';
 
 @Injectable()
 export class HttpErrorInterceptor implements HttpInterceptor {
 
   constructor(
     private translate: TranslateService,
-    private _dataService:DataService,
+    private _languageService: LanguageService,
+    private _dataService: DataService,
     private dialog: MatDialog
   ) {}
 
@@ -36,27 +37,22 @@ export class HttpErrorInterceptor implements HttpInterceptor {
       panelClass: "borderless-dialog"
     });
 
-    return next.handle(request).pipe(
+    // Clone request to add header
+    const modifiedRequest = request.clone({
+      setHeaders: {
+        "Accept-Language": this._languageService.getSelectedLanguage().value
+      }
+    });
+
+
+    return next.handle(modifiedRequest).pipe(
       tap({
         error: (err: HttpErrorResponse) => {
         // console.log('Error returned by the server/backend', err);
 
           if (err.status === 418) { 
-            // 418 (I AM A TEAPOT) = CUSTOM ERROR FROM BACKEND                   
-            const errorResponse: ErrorResponse = err.error;
-
-            const errorCode = errorResponse.errorCode;
-            const errorMessage = `I18N.CUSTOM_ERROR.${errorCode}`;
-            
-            const translatedErrorMessage = this.translate.instant(errorMessage);
-    
-            // Check if translation exists, if not, provide a fallback message
-            if(errorMessage == translatedErrorMessage) {
-              return this._dataService.openSnackBar_Error("Error not translated, yet...", 0);
-            }
-
-            this._dataService.openSnackBar_Error(translatedErrorMessage, 0);
-
+            const errorMessage:string = err.error;
+            this._dataService.openSnackBar_Error(errorMessage, 0);
           } else { // GENERAL ERRORS FROM BACKEND
 
             switch (err.status) {
@@ -64,19 +60,19 @@ export class HttpErrorInterceptor implements HttpInterceptor {
                 this._dataService.openSnackBar_Error(this.translate.instant("I18N.GENERAL_ERROR.0"), 0);
                 break;
 
-              case 400:
-                let exceptionMessage: string[] = [];
+              // case 400:
+              //   let exceptionMessage: string[] = [];
 
-                for (let index = 0; index < err.error.errors.length; index++) {
-                  const message: string = err.error.errors[index].defaultMessage;
-                  exceptionMessage.push(message);
-                }
+              //   for (let index = 0; index < err.error.errors.length; index++) {
+              //     const message: string = err.error.errors[index].defaultMessage;
+              //     exceptionMessage.push(message);
+              //   }
 
-                console.log(err);
-                console.log(err.error.errors);
+              //   console.log(err);
+              //   console.log(err.error.errors);
 
-                this._dataService.openSnackBar_Error(exceptionMessage.join('\n'), 0);
-                break;
+              //   this._dataService.openSnackBar_Error(exceptionMessage.join('\n'), 0);
+              //   break;
 
               case 404:
                 this._dataService.openSnackBar_Error(this.translate.instant("I18N.GENERAL_ERROR.404"), 0);
