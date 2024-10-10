071feb2be35cb2cfe68a5e863744fba8896419dc
Bogdan <cristea.ibogdan@gmail.com>
2024-10-10 15:27:10 +0300

    added form to identify the solar panel

--- src/app/solar-panel/components/solar-panel-identification/solar-panel-identification.component.ts
+++ src/app/solar-panel/components/solar-panel-identification/solar-panel-identification.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { NonNullableFormBuilder, Validators } from '@angular/forms';
+import { SerialNumberValidator } from 'src/app/shared/validators/async-validators/serial-number.validator';
+import { CustomValidators } from 'src/app/shared/validators/custom.validators';
 
 @Component({
   selector: 'app-solar-panel-identification',
@@ -6,5 +9,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./solar-panel-identification.component.css']
 })
 export class SolarPanelIdentificationComponent {
+  
+  constructor(
+    private fb: NonNullableFormBuilder,
+    private serialNumberValidator: SerialNumberValidator //TODO: Use async validator to check the serial number
+  ){}
+
+  solarPanelForm = this.fb.group({        
+    category: [{value:"Solar Panel", disabled:true}],
+    manufacturer: ["", Validators.required],
+
+    modelAndType: this.fb.group({
+      model: "",
+      type: ""
+    }, {validators: CustomValidators.atLeastOneControlRequired("model","type")}),
+
+    serialNumber: ["", {
+      validators: Validators.required
+    }],
+  });
 
 }
