afbf0c1b9264d1b3a1a471397bda8fe7a5166b3b
Bogdan <cristea.ibogdan@gmail.com>
2024-10-16 15:30:59 +0300

    Added custom validator. Added warning message

--- src/app/solar-panel/components/solar-panel-damage/solar-panel-damage.component.ts
+++ src/app/solar-panel/components/solar-panel-damage/solar-panel-damage.component.ts
@@ -3,6 +3,8 @@ import { NonNullableFormBuilder } from '@angular/forms';
 import { MatStepper } from '@angular/material/stepper';
 import { SolarPanelDamage } from '../../models/solar-panel-damage.model';
 import { SolarPanelService } from '../../services/solar-panel.service';
+import { CustomValidators } from 'src/app/shared/validators/custom.validators';
+import { NotificationService } from 'src/app/services/notification.service';
 
 @Component({
   selector: 'app-solar-panel-damage',
@@ -14,7 +16,8 @@ export class SolarPanelDamageComponent {
   constructor(
     @Inject(MatStepper) private stepper: MatStepper,
     private fb: NonNullableFormBuilder,
-    private _solarPanelService: SolarPanelService
+    private _solarPanelService: SolarPanelService,
+    private _notificationService: NotificationService
   ) { }
 
   solarPanelDamageForm = this.fb.group({    
@@ -23,6 +26,13 @@ export class SolarPanelDamageComponent {
     snailTrails: [false],
     brokenGlass: [false],
     additionalDetails: [""]
+  },{
+    validators: CustomValidators.atLeastOneOutOfFourTrue(
+    "hotSpots",
+    "microCracks",
+    "snailTrails",
+    "brokenGlass"
+    )
   });
 
   charactersMaxLimit: number = 200;
@@ -33,6 +43,7 @@ export class SolarPanelDamageComponent {
 
   onSubmit() {
     if(this.solarPanelDamageForm.invalid) {
+      this._notificationService.showError("At least one damage type has to be selected", 0);
       return;
     }
 
