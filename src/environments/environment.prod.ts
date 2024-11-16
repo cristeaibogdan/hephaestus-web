a550cda4183a4654b01d1bb555adfd2a11335f56
Bogdan <cristea.ibogdan@gmail.com>
2024-11-16 11:55:50 +0200

    added azure apiBaseUrl

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiBaseUrl: "https://api-gateway-8rxu.onrender.com"
+  apiBaseUrl: "https://api-gateway.orangesmoke-f68e1158.northeurope.azurecontainerapps.io"
 };
