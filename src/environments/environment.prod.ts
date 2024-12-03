116a6c3b5563a737f1052d183eb78b94dd68f8c7
Bogdan <cristea.ibogdan@gmail.com>
2024-12-03 14:31:57 +0200

    Added zipkin tracing

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiBaseUrl: "https://api-gateaway.kindmeadow-812476e6.northeurope.azurecontainerapps.io"
+  apiBaseUrl: "https://api-gateaway.kindmeadow-812476e6.northeurope.azurecontainerapps.io",
+  zipkinUrl: "https://zipkin.kindmeadow-812476e6.northeurope.azurecontainerapps.io/api/v2/spans"
 };
