48a35d874239ad5aeae23343d824752a1d5d6d1e
Bogdan <cristea.ibogdan@gmail.com>
2024-12-21 13:19:13 +0200

    update to angular 18

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,7 @@ import { HomeComponent } from './components/home/home.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule, provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideHttpClient, withInterceptors, withInterceptorsFromDi } from '@angular/common/http';
 import { RegisterComponent } from './components/register/register.component';
 import { LoginComponent } from './components/login/login.component';
 import { SharedModule } from './shared/shared.module';
@@ -19,57 +19,46 @@ import { CompositePropagatorModule, OpenTelemetryInterceptorModule, ZipkinExport
 import { environment } from 'src/environments/environment';
 import { WakeupComponent } from './components/wakeup/wakeup.component';
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    FooterComponent,
-    HeaderComponent,
-    
-    RegisterComponent,
-    LoginComponent,
-    WakeupComponent,
-  ],
-  imports: [    
-    SharedModule,
-
-    BrowserModule,
-    BrowserAnimationsModule,    
-
-    AppRoutingModule,    
-    HttpClientModule,
-
-  // Zipkin tracing configuration start
-    OpenTelemetryInterceptorModule.forRoot({
-      commonConfig: {
-        serviceName: 'hephaestus-web', // Service name sent in trace
-        console: false, // Display trace in console
-        probabilitySampler: '1', // 100% sampling
-      },
-      zipkinConfig: {
-        url: environment.zipkinUrl, // URL of Zipkin
-      },
-      ignoreUrls: {
-        urls: [/assets\/svgs\/.*/] // Ignore requests used to register svgs from assets/svgs
-      }
-    }),
-    ZipkinExporterModule, // Handles export to Zipkin
-    CompositePropagatorModule // Defines propagation in HTTP header
-  // Zipkin tracing configuration end
-  ],
-  providers: [
-    {
-      provide: ErrorHandler, 
-      useClass: GlobalErrorHandler
-    },
-    provideHttpClient(
-      withInterceptors([
-        timeoutInterceptor,
-        languageInterceptor,
-        loadingInterceptor
-      ])
-    )
-  ],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent,
+        HomeComponent,
+        FooterComponent,
+        HeaderComponent,
+        RegisterComponent,
+        LoginComponent,
+        WakeupComponent,
+    ],
+    bootstrap: [AppComponent], imports: [SharedModule,
+        BrowserModule,
+        BrowserAnimationsModule,
+        AppRoutingModule,
+        // Zipkin tracing configuration start
+        OpenTelemetryInterceptorModule.forRoot({
+            commonConfig: {
+                serviceName: 'hephaestus-web', // Service name sent in trace
+                console: false, // Display trace in console
+                probabilitySampler: '1', // 100% sampling
+            },
+            zipkinConfig: {
+                url: environment.zipkinUrl, // URL of Zipkin
+            },
+            ignoreUrls: {
+                urls: [/assets\/svgs\/.*/] // Ignore requests used to register svgs from assets/svgs
+            }
+        }),
+        ZipkinExporterModule, // Handles export to Zipkin
+        CompositePropagatorModule // Defines propagation in HTTP header
+        // Zipkin tracing configuration end
+    ], providers: [
+        {
+            provide: ErrorHandler,
+            useClass: GlobalErrorHandler
+        },
+        provideHttpClient(withInterceptors([
+            timeoutInterceptor,
+            languageInterceptor,
+            loadingInterceptor
+        ])),
+        provideHttpClient(withInterceptorsFromDi())
+    ] })
 export class AppModule { }
