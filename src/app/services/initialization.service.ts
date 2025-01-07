1ca18bf232fc2a876859d03942bb0d898b3a8715
Bogdan <cristea.ibogdan@gmail.com>
2025-01-07 15:35:50 +0200

    Added retry and console.warn for each retry

--- src/app/services/initialization.service.ts
+++ src/app/services/initialization.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
 import { Router } from '@angular/router';
-import { firstValueFrom } from 'rxjs';
+import { firstValueFrom, retry, tap, timer } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
 @Injectable({ providedIn: 'root' })
@@ -18,7 +18,15 @@ export class InitializationService {
     
     try { // First request to wakeup washing-machine service
       await firstValueFrom(
-        this.http.get(`${this.apiUrl}/api/v1/washing-machines/someSerialNumber/validate`)
+        this.http.get(`${this.apiUrl}/api/v1/washing-machines/someSerialNumber/validate`).pipe(
+          retry({
+            count: 3,
+            delay: (error, count) => {
+              console.warn(`Washing Machine retry attempt ${count}/3`, error);
+              return timer(0);
+            }
+          })
+        )
       );
       washingMachineAwake = true;
     } catch (err) {
@@ -27,7 +35,15 @@ export class InitializationService {
     
     try { // Second request to wakeup washing-machine service
       await firstValueFrom(
-        this.http.get(`${this.apiUrl}/api/v1/products/Washing Machine/manufacturers`)
+        this.http.get(`${this.apiUrl}/api/v1/products/Washing Machine/manufacturers`).pipe(
+          retry({
+            count: 3,
+            delay: (error, count) => {
+              console.warn(`Product retry attempt ${count}/3`, error);
+              return timer(0);
+            }
+          })
+        )
       );
       productAwake = true;
     } catch (err) {
