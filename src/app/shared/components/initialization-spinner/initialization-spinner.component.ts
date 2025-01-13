e0795edeefe15e2aab25f35f9d981914bfd1d568
Bogdan <cristea.ibogdan@gmail.com>
2025-01-13 12:07:48 +0200

    added countdown

--- src/app/shared/components/initialization-spinner/initialization-spinner.component.ts
+++ src/app/shared/components/initialization-spinner/initialization-spinner.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject, inject } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import {
   MatDialogTitle,
@@ -7,10 +7,13 @@ import {
 } from '@angular/material/dialog';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
+import { map, take, timer } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-initialization-spinner',
   imports: [
+    CommonModule,
     MatProgressSpinnerModule,
     MatDialogTitle,
     MatDialogContent,
@@ -22,4 +25,13 @@ import { MatButtonModule } from '@angular/material/button';
 })
 export class InitializationSpinnerComponent { 
   data: any = inject(MAT_DIALOG_DATA);
+
+  private COUNTDOWN_TIME = 3 * 60;
+
+  countdown$ = timer(0, 1000).pipe( // Starts immediately (0 delay) and emits every second
+    take(this.COUNTDOWN_TIME + 1), // Emits 181 times (from 180 to 0)
+    // Countdown in ms as the date pipe accepts only ms
+    map((elapsedSeconds) => (this.COUNTDOWN_TIME - elapsedSeconds) * 1000) 
+  );
+
 }
