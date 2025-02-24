0099f2284d12473cec018c18faf89b906b172dc5
Bogdan <cristea.ibogdan@gmail.com>
2025-02-24 16:33:19 +0200

    Extraced upload image functionality in one component

--- src/app/washing-machine/components/washing-machine-damage/washing-machine-damage.component.ts
+++ src/app/washing-machine/components/washing-machine-damage/washing-machine-damage.component.ts
@@ -1,5 +1,4 @@
 import { Component, inject } from '@angular/core';
-import { DomSanitizer } from '@angular/platform-browser';
 import { NonNullableFormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { CustomValidators } from '../../../shared/validators/custom.validators';
 import { ImageFile } from 'src/app/washing-machine/models/image-file.model';
@@ -7,18 +6,16 @@ import { WashingMachineService } from '../../services/washing-machine.service';
 import { NotificationService } from 'src/app/services/notification.service';
 import { MatStepper, MatStepperModule } from '@angular/material/stepper';
 import { WashingMachineDetail } from '../../models/washing-machine-detail.model';
-import { TranslocoModule, TranslocoService } from '@jsverse/transloco';
+import { TranslocoModule } from '@jsverse/transloco';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { MatTooltipModule } from '@angular/material/tooltip';
 import { CommonModule } from '@angular/common';
-import { DragAndDropDirective } from 'src/app/shared/directives/drag-and-drop.directive';
 import { StepperButtonsDirective } from 'src/app/shared/directives/stepper-buttons.directive';
 import { PricingFormComponent } from './pricing-form/pricing-form.component';
 import { PackageFormComponent } from './package-damage-form/package-form.component';
 import { VisibleSurfacesFormComponent } from './visible-surfaces-form/visible-surfaces-form.component';
 import { HiddenSurfacesFormComponent } from './hidden-surfaces-form/hidden-surfaces-form.component';
+import { FileUploadComponent } from './file-upload/file-upload.component';
 
 @Component({
   selector: 'app-washing-machine-damage',
@@ -27,20 +24,18 @@ import { HiddenSurfacesFormComponent } from './hidden-surfaces-form/hidden-surfa
   imports: [
     MatCardModule,
     MatButtonModule,
-    MatIconModule,
-    MatTooltipModule,
     MatStepperModule, // for the directive matStepperPrevious
 
     CommonModule,
-    ReactiveFormsModule,
     TranslocoModule,
-    DragAndDropDirective,
+    ReactiveFormsModule,
     StepperButtonsDirective,
 
-    PricingFormComponent,
+    FileUploadComponent,
     PackageFormComponent,
     VisibleSurfacesFormComponent,
-    HiddenSurfacesFormComponent
+    HiddenSurfacesFormComponent,
+    PricingFormComponent,
   ]
 })
 export class WashingMachineDamageComponent {  
@@ -48,8 +43,6 @@ export class WashingMachineDamageComponent {
   private fb = inject(NonNullableFormBuilder);
   private _washingMachineService = inject(WashingMachineService);
   private _notifService = inject(NotificationService);
-  private _translocoService = inject(TranslocoService);
-  private sanitizer = inject(DomSanitizer);
   
   washingMachineDetailForm = this.fb.group({
     applicablePackageDamage: [false],
@@ -133,11 +126,17 @@ export class WashingMachineDamageComponent {
     "repairPrice",
     "price")}
   );
+
+  selectedFiles:ImageFile[] = [];
     
+  private resetSelectedFiles(): void {
+    this.selectedFiles = [];
+  }
+
 // *******************************
 // *** FORM RESET AND SUBMIT
 // *******************************
-  
+
   onReset(e:Event): void {
     e.preventDefault();
     this.washingMachinePricingForm.reset();
@@ -203,96 +202,4 @@ export class WashingMachineDamageComponent {
     this._washingMachineService.setSelectedFiles(this.selectedFiles);
     this.stepper.next();
   }
-
-
-// *****************************
-// *** MULTIPLE PICTURES
-// *****************************
-
-  selectedFiles:ImageFile[] = [];
-
-  onDrop(droppedFiles:FileList): void {
-    this.processFiles(droppedFiles);
-  }
-
-  onFileUpload(event: any): void {
-    // console.log(event);
-    const htmlInput = event.target as HTMLInputElement;
-    if(htmlInput.files) {
-      this.processFiles(htmlInput.files);
-    }
-  }
-
-  private processFiles(files: FileList): void {    
-    // 1. Validate file length
-    const totalFilesCount = this.selectedFiles.length + files.length;
-    if (totalFilesCount > 3) {
-      this._notifService.showError(this._translocoService.translate("I18N.CUSTOM_ERROR.IMAGE_LIMIT"),0);
-      return;
-    }
-      
-    for (let i = 0; i < files.length; i++) {
-      const uploadedFile:File = files[i];      
-      
-      // 2. Validate file extension
-      if(this.invalidFileExtension(uploadedFile.name)) {
-        this._notifService.showError(
-          this._translocoService.translate("I18N.CUSTOM_ERROR.IMAGE_FILE")
-          +" "+uploadedFile.name+" "+
-          this._translocoService.translate("I18N.CUSTOM_ERROR.IMAGE_EXTENSION_TEXT"),0);
-        return;
-      }
-      
-      // 3. Validate file size, must not exceed 3 MB
-      if(this.invalidFileSize(uploadedFile.size, 3)) {
-        this._notifService.showError(
-          this._translocoService.translate("I18N.CUSTOM_ERROR.IMAGE_FILE")
-          +" "+uploadedFile.name+" "+
-          this._translocoService.translate("I18N.CUSTOM_ERROR.IMAGE_SIZE_TEXT"),0);
-        return;
-      }
-            
-      const imageFile:ImageFile = {
-        file: uploadedFile,
-        url: this.sanitizer.bypassSecurityTrustUrl(
-          URL.createObjectURL(uploadedFile)
-        )
-      }
-
-      this.selectedFiles.push(imageFile);  
-    }    
-  }
-
-  onRemoveImage(index:number): void {
-    this.selectedFiles.splice(index, 1);
-  }
-
-  resetSelectedFiles(): void {
-    this.selectedFiles = [];
-  }
-
-//************************
-//***** HELPER METHODS
-//************************
-  
-  invalidFileExtension(fileName: string): boolean {
-    const extension = fileName.substring(fileName.lastIndexOf('.') + 1);
-  
-    switch(extension.toLowerCase()) {
-      case 'png': return false;  
-      case 'jpg': return false;
-      case 'jpeg': return false;
-      case 'bmp': return false;
-
-      default: return true;
-    }
-  }
-
-  invalidFileSize(fileSize:number, maxFileSizeInMB:number): boolean {
-    const fileSizeInMB:number = fileSize / (1024 * 1024);
-    return (fileSizeInMB < maxFileSizeInMB) 
-      ? false 
-      : true;
-  }
-
 }
