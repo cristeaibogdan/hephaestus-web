013ff8ded8dc36feb1ae16b5f27074c59545e424
Bogdan <cristea.ibogdan@gmail.com>
2025-05-11 12:02:18 +0300

    Moved detail and image inside GetWashingMachineFullResponse

--- src/app/washing-machine/models/dtos/get-washing-machine-full.response.ts
+++ src/app/washing-machine/models/dtos/get-washing-machine-full.response.ts
@@ -2,8 +2,6 @@ import { DamageType } from '../../enums/damage-type.enum';
 import { IdentificationMode } from '../../enums/identification-mode.enum';
 import { Recommendation } from '../../enums/recommendation.enum';
 import { ReturnType } from '../../enums/return-type.enum';
-import { GetWashingMachineDetailResponse } from './get-washing-machine-detail.response';
-import { GetWashingMachineImageResponse } from './get-washing-machine-image.response';
 
 export interface GetWashingMachineFullResponse {
   category: string,
@@ -20,6 +18,54 @@ export interface GetWashingMachineFullResponse {
   recommendation: Recommendation,
   createdAt: Date,
 
-  washingMachineDetail: GetWashingMachineDetailResponse,
-  washingMachineImages: GetWashingMachineImageResponse[]
+  washingMachineDetail: WashingMachineDetail,
+  washingMachineImages: WashingMachineImage[]
+}
+
+export interface WashingMachineDetail {
+
+    // PACKAGE
+    applicablePackageDamage: boolean;
+    packageDamaged: boolean;
+    packageDirty: boolean;
+    packageMaterialAvailable: boolean;
+
+    // VISIBLE SURFACES
+    applicableVisibleSurfacesDamage: boolean;
+
+    visibleSurfacesHasScratches: boolean;
+    visibleSurfacesScratchesLength: number;
+
+    visibleSurfacesHasDents: boolean;
+    visibleSurfacesDentsDepth: number;
+
+    visibleSurfacesHasMinorDamage: boolean;
+    visibleSurfacesMinorDamage: string;
+
+    visibleSurfacesHasMajorDamage: boolean;
+    visibleSurfacesMajorDamage: string;
+
+    // HIDDEN SURFACES
+    applicableHiddenSurfacesDamage: boolean;
+
+    hiddenSurfacesHasScratches: boolean;
+    hiddenSurfacesScratchesLength: number;
+
+    hiddenSurfacesHasDents: boolean;
+    hiddenSurfacesDentsDepth: number;
+
+    hiddenSurfacesHasMinorDamage: boolean;
+    hiddenSurfacesMinorDamage: string;
+
+    hiddenSurfacesHasMajorDamage: boolean;
+    hiddenSurfacesMajorDamage: string;
+
+    // PRICING
+    price: number;
+    repairPrice: number;
+}
+
+export interface WashingMachineImage {
+  imagePrefix: string;
+  image: string;
 }
--- src/app/washing-machine/models/dtos/get-washing-machine-image.response.ts
+++
@@ -1,4 +0,0 @@
-export interface GetWashingMachineImageResponse {
-  imagePrefix: string,
-  image: string
-}
