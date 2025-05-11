9a7a98399a7a7a16d97a4bf970d7fa19e8502bef
Bogdan <cristea.ibogdan@gmail.com>
2025-05-11 11:02:17 +0300

    Moved WashingMachineDetail inside SaveWashingMachineRequest. Modified name to match backend dto name

---
+++ src/app/washing-machine/models/dtos/save-washing-machine.request.ts
@@ -0,0 +1,42 @@
+import { DamageType } from '../../enums/damage-type.enum';
+import { IdentificationMode } from '../../enums/identification-mode.enum';
+import { ReturnType } from '../../enums/return-type.enum';
+
+export interface SaveWashingMachineRequest {
+  category: string;
+  identificationMode: IdentificationMode;
+  manufacturer: string;
+
+  model: string;
+  type: string;
+  serialNumber: string;
+
+  returnType: ReturnType;
+  damageType: DamageType;
+
+  washingMachineDetail: WashingMachineDetail;
+}
+
+export interface WashingMachineDetail {
+
+    // PACKAGE
+    packageDamaged: boolean;
+    packageDirty: boolean;
+    packageMaterialAvailable: boolean;
+
+    // VISIBLE SURFACES
+    visibleSurfacesScratchesLength: number;
+    visibleSurfacesDentsDepth: number;
+    visibleSurfacesMinorDamage: string;
+    visibleSurfacesMajorDamage: string;
+
+    // HIDDEN SURFACES
+    hiddenSurfacesScratchesLength: number;
+    hiddenSurfacesDentsDepth: number;
+    hiddenSurfacesMinorDamage: string;
+    hiddenSurfacesMajorDamage: string;
+
+    // PRICING
+    price: number;
+    repairPrice: number;
+}
--- src/app/washing-machine/services/washing-machine.service.ts
+++ src/app/washing-machine/services/washing-machine.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, inject } from "@angular/core";
 import { BehaviorSubject, EMPTY, Observable, switchMap, withLatestFrom} from "rxjs";
-import { SaveWashingMachineRequest } from "../models/dtos/create-washing-machine.request";
+import { SaveWashingMachineRequest } from "../models/dtos/save-washing-machine.request";
 import { ImageFile } from "../models/image-file.model";
 import { WashingMachineIdentification } from "../models/washing-machine-identification.model";
 import { WashingMachineDataService } from "./washing-machine.data.service";
@@ -86,7 +86,7 @@ export class WashingMachineService {
       model: this.washingMachineIdentification$.value.model,
       type: this.washingMachineIdentification$.value.type,
       
-      saveWashingMachineDetailRequest: {
+      washingMachineDetail: {
         packageDamaged: this.washingMachineDetail$.value.packageDamaged,
         packageDirty: this.washingMachineDetail$.value.packageDirty,
         packageMaterialAvailable: this.washingMachineDetail$.value.packageMaterialAvailable,
