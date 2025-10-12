556bedca5e5df6698b2fc382d4ea694110bfc3aa
Bogdan <cristea.ibogdan@gmail.com>
2025-10-12 15:01:59 +0300

    Extracted logic into self-explanatory methods

--- src/app/washing-machine/components/washing-machine-damage/package-damage-form/package-form.component.ts
+++ src/app/washing-machine/components/washing-machine-damage/package-damage-form/package-form.component.ts
@@ -1,4 +1,3 @@
-
 import { Component, DestroyRef, Input, OnInit, inject } from '@angular/core';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
@@ -25,35 +24,40 @@ export class PackageFormComponent implements OnInit {
   private destroyRef = inject(DestroyRef);
 
   ngOnInit(): void {
-    // 1. If false from the start, reset and disable packageForm
+    // If false from the start, reset and disable packageForm
     if(!this.applicablePackageDamage.value) {
       this.packageForm.reset();
       this.packageForm.disable({emitEvent: false});
     }
 
-    // 2. On every value change enable, reset and disable accordingly
-    this.applicablePackageDamage.valueChanges.pipe(
-      takeUntilDestroyed(this.destroyRef)
-    ).subscribe(value =>{
-      if(value) {
-        this.packageForm.enable({emitEvent: false});
-        this.packageForm.controls.packageMaterialAvailable.disable();
-      } else {
-        this.packageForm.reset();
-        this.packageForm.disable({emitEvent: false});
-      }
-    });
+    this.togglePackageFormBasedOnApplicablePackageDamage();
+    this.togglePackageMaterialAvailableBasedOnDamagedOrDirty();    
+  }
 
-    // 3. If packageDamaged or packageDirty is true, enable packageMaterialAvailable
-    this.packageForm.valueChanges.pipe(
-      takeUntilDestroyed(this.destroyRef)
-    ).subscribe(value => {
-      if(value.damaged || value.dirty) {
-        this.packageForm.controls.packageMaterialAvailable.enable({emitEvent: false});       
-      } else {
-        this.packageForm.controls.packageMaterialAvailable.setValue(false, {emitEvent: false});
-        this.packageForm.controls.packageMaterialAvailable.disable({emitEvent: false}); 
-      }
-    });
+  private togglePackageFormBasedOnApplicablePackageDamage() {
+    this.applicablePackageDamage.valueChanges
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe(value => {
+        if (value) {
+          this.packageForm.enable({ emitEvent: false });
+          this.packageForm.controls.packageMaterialAvailable.disable();
+        } else {
+          this.packageForm.reset();
+          this.packageForm.disable({ emitEvent: false });
+        }
+      });
+  }
+
+  private togglePackageMaterialAvailableBasedOnDamagedOrDirty() {
+    this.packageForm.valueChanges
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe(value => {
+        if (value.damaged || value.dirty) {
+          this.packageForm.controls.packageMaterialAvailable.enable({ emitEvent: false });
+        } else {
+          this.packageForm.controls.packageMaterialAvailable.setValue(false, { emitEvent: false });
+          this.packageForm.controls.packageMaterialAvailable.disable({ emitEvent: false });
+        }
+      });
   }
 }
